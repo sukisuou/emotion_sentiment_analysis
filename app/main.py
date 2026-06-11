510a19633df8f19e1e98af97244886cfd1a9c21d
sukisuou <adamdiey3@gmail.com>
2026-06-11 16:19:42 +0800

    wahhh

--- app/main.py
+++ app/main.py
@@ -18,16 +18,28 @@ def predict(texts):
     return model.serve(texts_arr).numpy()
 
 # ----- test inputs -----
+user_input = False
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
 
     texts = []
-    print("Enter your comment ('x' to exit):")
-    text = input()
-    if text == "x":
-        print("Bye bye~")
-        break
-    texts.append(text)
+    if user_input:
+        print("Enter your comment ('x' to exit):")
+        text = input()
+        if text == "x":
+            print("Bye bye~")
+            break
+        texts.append(text)
+    else:
+        texts = [
+            "honestly this is one of the best guides i've read in a while, thanks for taking the time to write it",
+            "what the hell is this update, literally nobody asked for this and somehow everything is worse now",
+            "am i the only one who doesn't understand what's going on here?",
+            "for anyone wondering, the answer is in the second paragraph of the guide",
+            "i hated that movie, but the ending made it worth it",
+            "honestly i don't know if this will work, but im hopeful",
+            "wow thanks, that solved absolutely none of my problems"
+        ]
 
     # predict
     print('\nAnalysing comment...')
@@ -55,6 +67,10 @@ while True:
             highest_idx = np.argmax(probs)
             emotion_name = emotion_labels[highest_idx]
             print(f'   - {emotion_name}: {probs[highest_idx] * 100:.2f}% (Highest, below threshold)')
+        print()
 
+    if user_input:
         print('\nPress enter to continue...', end = "")
         input()
+    else:
+        break
